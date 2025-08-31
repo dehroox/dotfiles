514f6680607affb5a1379292f8b89327ca63af30
dehroox <dehrexd@gmail.com>
2025-08-31 19:47:35 +0800

    fzf-native, and blink.cmp instead of coq

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -56,6 +56,9 @@ if not vim.uv.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Misc opt
+vim.opt.completeopt = { "menuone", "noselect" }
+
 -- Plugins
 require("lazy").setup({
 
@@ -93,15 +96,30 @@ require("lazy").setup({
 
     -- Telescope
     {
-       "nvim-telescope/telescope.nvim",
+        "nvim-telescope/telescope.nvim",
         cmd = "Telescope",
-        dependencies = { "nvim-lua/plenary.nvim" },
+        dependencies = { 
+            "nvim-lua/plenary.nvim",
+            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+        },
         config = function()
             require("telescope").setup({
                 pickers = {
                     find_files = { theme = "dropdown" },
                 },
+                defaults = {
+                    file_ignore_patterns = {"node_modules", ".git"},
+                    prompt_prefix = "🔍 ",
+                },
+                extensions = {
+                    fzf = {},
+                },
+                highlight = { enable = true },
+                incremental_selection = { enable = true },
+                textobjects = { enable = true },
             })
+
+            require('telescope').load_extension("fzf")
         end,
     },
 
@@ -121,29 +139,35 @@ require("lazy").setup({
         config = true,
     },
 
-    -- Autocomplete (Coq)
+    -- Autocomplete
     {
-        "ms-jpq/coq_nvim",
-        branch = "coq",
-        dependencies = {
-            "ms-jpq/coq.artifacts",
-            "ms-jpq/coq.thirdparty"
+        'saghen/blink.cmp',
+        dependencies = { 'rafamadriz/friendly-snippets' },
+        version = '*',
+
+        ---@module 'blink.cmp'
+        ---@type blink.cmp.Config
+        opts = {
+            keymap = { preset = 'default' },
+
+            appearance = {
+                nerd_font_variant = 'mono'
+            },
+
+            completion = { documentation = { auto_show = true, auto_show_delay_ms = 500, } },
+
+            sources = {
+                default = { 'lsp', 'path', 'snippets', 'buffer' },
+            },
+
+            fuzzy = { implementation = "prefer_rust_with_warning" }
         },
-        config = function()
-            vim.opt.completeopt = { "menuone", "noselect" }
-
-            vim.g.coq_settings = {
-                auto_start = 'shut-up'
-            }
-
-            require("coq")
-        end,
-    },
+        opts_extend = { "sources.default" }
+    }
 })
 
 -- LSP
-local lsp_capabilities = vim.lsp.protocol.make_client_capabilities()
-lsp_capabilities =  require("coq").lsp_ensure_capabilities(lsp_capabilities)
+local lsp_capabilities = require('blink.cmp').get_lsp_capabilities()
 
 vim.lsp.config("*", {
     capabilities = lsp_capabilities,
