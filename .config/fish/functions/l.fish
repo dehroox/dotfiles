2f64f85b8dd4c70479f32d1728b4e9802032e891
dehroox <dehrexd@gmail.com>
2025-09-03 06:38:16 +0800

    blahbleh

--- .config/fish/functions/l.fish
+++
@@ -1,4 +0,0 @@
-function l --wraps='eza -lhbo -rs=date --icons --nopermissions' --wraps='eza -lhbo -rs=date --icons --no-permissions' --description 'alias l eza -lhbo -rs=date --icons --no-permissions'
-  eza -lhbo -rs=date --icons --no-permissions $argv
-        
-end
